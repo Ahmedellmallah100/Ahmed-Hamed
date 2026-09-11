b7c8b5a8e33ddb555fc9f2e9f73d4dc236362e15
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-11 20:39:38 +0300

    Delete src directory

--- src/PCplus4Adder.v
+++
@@ -1,7 +0,0 @@
-//Adder for PC+1. The module name is kept from the original design.
-module PCplus4Adder (
-    input  [7:0] FromPC,
-    output [7:0] NextPC
-);
-    assign NextPC = FromPC + 8'd1;
-endmodule
